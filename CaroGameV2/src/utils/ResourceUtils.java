b2fbf030197daa27ca7ad8f534d9e8946092f03a
Thuy Hong <dthong.p20@ifi.edu.vn>
2016-10-17 11:16:53 +0700

    v2

---
+++ CaroGameV2/src/utils/ResourceUtils.java
@@ -0,0 +1,10 @@
+package utils;
+
+public class ResourceUtils {
+	public static final int LENGTH_BOARD = 15;
+	public static final int WIDTH_BOARD = 15;
+	
+	public static final String ICON_X = "cross.gif";
+	public static final String ICON_O = "nought.gif";
+	public static final String BACKGROUND_CELL = "blank.gif";
+}
